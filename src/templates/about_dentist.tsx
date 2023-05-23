334d6b209bf27042c685ee2fb301828c33306c75
rkeerthi <sunnykeerthi@gmail.com>
2023-05-23 13:51:43 +0530

    fixed

--- src/templates/about_dentist.tsx
+++ src/templates/about_dentist.tsx
@@ -98,6 +98,9 @@ const AboutYourDentist: Template<TemplateRenderProps> = ({
     <>
       <Schema document={cpy}></Schema>
       <PageLayout _site={_site}>
+        <div className="mx-auto my-16 text-center text-xl md:text-3xl underline">
+          Meet our Dentists
+        </div>
         <div className="centered-container">
           <div className="section space-y-10">
             {c_relatedDoctors.map((item: any, index: any) => {
@@ -112,41 +115,43 @@ const AboutYourDentist: Template<TemplateRenderProps> = ({
               } = item;
 
               return (
-                <div
-                  className="flex flex-row justify-evenly space-x-4"
-                  key={index}
-                >
-                  <div className="!w-1/4">
-                    <Image image={headshot}></Image>
+                <>
+                  <div
+                    className="flex flex-col md:flex-row justify-evenly space-y-4 md:space-x-4"
+                    key={index}
+                  >
+                    <div className="w-full md:!w-1/4">
+                      <Image image={headshot}></Image>
+                    </div>
+                    <div className="w-full md:w-3/4 py-4 flex flex-col justify-between space-y-2">
+                      <div>
+                        <div className="text-2xl md:text-3xl  ">{name}</div>
+                        <hr className="mt-2 " />
+                      </div>
+                      <div className="flex flex-row space-x-4 ">
+                        <div className="gap-2 items-center px-2 py-1 font-medium flex justify-start">
+                          <PhoneIcon className="h-3 w-3" />
+                          {mainPhone}
+                        </div>
+                        <div className="gap-2 items-center px-2 py-1 font-medium flex justify-start">
+                          <AtSymbolIcon className="h-3 w-3" />
+                          {"person@email.com"}
+                        </div>
+                      </div>
+                      <div className="w-full md:w-auto">
+                        {description && description.length >= 400
+                          ? description.slice(0, 400)
+                          : description ||
+                            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias repellendus vero eligendi esse qui nam veritatis, corrupti aperiam sequi fuga cupiditate magnam doloribus? Rem mollitia magnam cumque eos in fuga?"}
+                      </div>
+                      <a href={slug}>
+                        <div className="mt-4 md:mt-0 border  py-2 px-4 bg-gray-600 text-white w-fit   hover:bg-white hover:text-gray-600 hover:border-gray-600">
+                          Learn more
+                        </div>
+                      </a>
+                    </div>
                   </div>
-                  <div className="w-3/4 py-4 flex flex-col justify-between">
-                    <div>
-                      <div className="text-3xl font-bold">{name}</div>
-                      <hr className="mt-2 " />
-                    </div>
-                    <div className="flex flex-row space-x-4">
-                      <div className="gap-2 items-center px-2 py-1 font-medium flex justify-start">
-                        <PhoneIcon className="h-3 w-3" />
-                        {mainPhone}
-                      </div>
-                      <div className="gap-2 items-center px-2 py-1 font-medium flex justify-start">
-                        <AtSymbolIcon className="h-3 w-3" />
-                        {"person@email.com"}
-                      </div>
-                    </div>
-                    <div>
-                      {description && description.length >= 400
-                        ? description.slice(0, 400)
-                        : description ||
-                          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias repellendus vero eligendi esse qui nam veritatis, corrupti aperiam sequi fuga cupiditate magnam doloribus? Rem mollitia magnam cumque eos in fuga?"}
-                    </div>
-                    <a href={slug}>
-                      <div className=" border  py-2 px-4 bg-gray-600 text-white w-fit   hover:bg-white hover:text-gray-600 hover:border-gray-600">
-                        Learn more
-                      </div>
-                    </a>
-                  </div>
-                </div>
+                </>
               );
             })}
           </div>
